a20875fdba5d992a9fa1f2b60965f49e7d0fa5aa
Harry Potter <84266473+LIUQIMINGXI@users.noreply.github.com>
2024-10-18 03:11:50 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  serverRuntimeConfig: {
+    serverIP: '43.128.143.138', // 将 'your_vps_public_ip' 替换成你的 VPS 公网 IP 地址
+  },
+};
 export default nextConfig;
